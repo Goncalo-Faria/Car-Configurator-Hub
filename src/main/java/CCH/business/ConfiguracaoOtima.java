61140fe3d7dddbd6fa18453f3c5e88f2e6409e33
Felfit <31731191+Felfit@users.noreply.github.com>
2018-12-24 19:30:37 +0000

    Update ConfiguracaoOtima.java

--- src/main/java/CCH/business/ConfiguracaoOtima.java
+++ src/main/java/CCH/business/ConfiguracaoOtima.java
@@ -9,6 +9,61 @@ import java.util.List;
 
 public class ConfiguracaoOtima {
 
+    private void restricaoComponentes(IloCplex cplex, List<Componente> componentesObrigatorios, List<Componente> componentes, HashMap<Integer, IloIntVar> comps) throws IloException {
+        for (Componente c:componentes) {
+            IloIntVar value = comps.get(c.getId());
+            List<Integer> incomp = c.getIdIncompativeis();
+            for (Integer k:incomp) {
+                if (comps.containsKey(k) && c.getId() < k){
+                    IloIntVar kvalue = comps.get(k);
+                    cplex.addLe(cplex.sum(value,kvalue),1);
+                }
+                else
+                if (c.getId() > k)//otimização para não repetir restrições uma vez que a incompatibilidade funciona nas duas direções
+                    cplex.addEq(0,value);//se o componente não estiver em comps o modelo não o seleciona, Para prevenir Erros.
+            }
+            List<Integer> necessarios = c.getIdNecessarios();
+            for (Integer k:necessarios) {
+                if (comps.containsKey(k)){
+                    IloIntVar kvalue = comps.get(k);
+                    cplex.addLe(value,kvalue);//se a precisa de b entao a<=b
+                }
+                else cplex.addEq(0,value);//se o componente não estiver em comps o modelo não o seleciona, Para prevenir Erros.
+            }
+        }
+
+        //componentes obrigatórios
+        for (Componente c:componentesObrigatorios) {
+            if(!comps.containsKey(c.getId()))throw new IloException();
+            cplex.addEq(comps.get(c.getId()),1);//garante que os componentes obrigatórios são selecionados
+            //está aqui para garantir que não ocorrem incompatibilidades
+        }
+    }
+
+    private void restricaoPacotes(IloCplex cplex, List<Pacote> pacotes, List<Componente> componentes, HashMap<Integer, IloIntVar> comps, HashMap<Integer, IloIntVar> pacs) throws IloException{
+        HashMap<Integer,IloNumExpr> overlap= new HashMap();
+        for (Pacote p:pacotes) {
+            IloIntVar pvalue = pacs.get(p.getId());
+            List<Integer> cs = p.idsComponentes();
+            for (Integer k:cs) {
+                if (comps.containsKey(k)){
+                    IloIntVar kvalue = comps.get(k);
+                    cplex.addLe(pvalue,kvalue);//se a precisa de b entao a<=b
+                    // construção das restrições de overlap de pacotes
+                    if(!overlap.containsKey(k)){
+                        overlap.put(k,cplex.constant(0));
+                    }
+                    IloNumExpr exp = overlap.get(k);
+                    exp = cplex.sum(exp,pvalue);
+                }
+                else cplex.addEq(0,pvalue);//se o componente não estiver em comps o modelo não o seleciona, Para prevenir Erros.
+            }
+        }
+        for (IloNumExpr exp:overlap.values())
+            cplex.addLe(exp,1);//apenas um pacote pode conter um componente
+    }
+
+
     public Configuracao configuracaoOtima(List<Componente> componentesObrigatorios,List<Componente> componentes, List<Pacote> pacotes,double money) throws IloException {
         IloCplex cplex = new IloCplex();
         HashMap<Integer,IloIntVar> comps = new HashMap<>();
@@ -33,60 +88,12 @@ public class ConfiguracaoOtima {
         //Função Objetivo
         cplex.addMaximize(objfunc);
 
-        //restrições Componentes
-        for (Componente c:componentes) {
-            IloIntVar value = comps.get(c.getId());
-            List<Integer> incomp = c.getIdIncompativeis();
-            for (Integer k:incomp) {
-                if (comps.containsKey(k) && c.getId() < k){
-                    IloIntVar kvalue = comps.get(k);
-                    cplex.addLe(cplex.sum(value,kvalue),1);
-                }
-                else
-                    if (c.getId() > k)//otimização para não repetir restrições uma vez que a incompatibilidade funciona nas duas direções
-                        cplex.addEq(0,value);//se o componente não estiver em comps o modelo não o seleciona, Para prevenir Erros.
-            }
-            List<Integer> necessarios = c.getIdNecessarios();
-            for (Integer k:necessarios) {
-                if (comps.containsKey(k)){
-                    IloIntVar kvalue = comps.get(k);
-                    cplex.addLe(value,kvalue);//se a precisa de b entao a<=b
-                }
-                else cplex.addEq(0,value);//se o componente não estiver em comps o modelo não o seleciona, Para prevenir Erros.
-            }
-        }
-
-        //componentes obrigatórios
-        for (Componente c:componentesObrigatorios) {
-            if(!comps.containsKey(c.getId()))throw new IloException();
-            cplex.addEq(comps.get(c.getId()),1);//garante que os componentes obrigatórios são selecionados
-            //está aqui para garantir que não ocorrem incompatibilidades
-        }
-
-        //restrições de pacotes
-        HashMap<Integer,IloNumExpr> overlap= new HashMap();
-        for (Pacote p:pacotes) {
-            IloIntVar pvalue = pacs.get(p.getId());
-            List<Integer> cs = p.idsComponentes();
-            for (Integer k:cs) {
-                if (comps.containsKey(k)){
-                    IloIntVar kvalue = comps.get(k);
-                    cplex.addLe(pvalue,kvalue);//se a precisa de b entao a<=b
-                    // construção das restrições de overlap de pacotes
-                    if(!overlap.containsKey(k)){
-                        overlap.put(k,cplex.constant(0));
-                    }
-                    IloNumExpr exp = overlap.get(k);
-                    exp = cplex.sum(exp,pvalue);
-                }
-                else cplex.addEq(0,pvalue);//se o componente não estiver em comps o modelo não o seleciona, Para prevenir Erros.
-            }
-        }
-        for (IloNumExpr exp:overlap.values())
-            cplex.addLe(exp,1);//apenas um pacote pode conter um componente
+        //restrições de pacotes e componentes
+        restricaoComponentes(cplex,componentesObrigatorios,componentes,comps);
+        restricaoPacotes(cplex,pacotes,componentes,comps,pacs);
 
         //restrição de preço
-            cplex.addLe(objfunc,money);
+            cplex.addLe(money,objfunc);
 
         //resolve o problema
         if(cplex.solve()){
