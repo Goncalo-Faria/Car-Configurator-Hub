eba973be5b18b154479712d5362facb7849607c0
Catarina Machado <catarinamachado11@gmail.com>
2018-12-26 16:44:43 +0000

    view todos os componentes

--- src/main/java/CCH/controller/admin/AdicionarComponentesPacoteController.java
+++ src/main/java/CCH/controller/admin/AdicionarComponentesPacoteController.java
@@ -6,6 +6,7 @@ import CCH.business.Componente;
 import CCH.business.Pacote;
 import CCH.exception.ComponenteIncompativelNoPacoteException;
 import CCH.exception.ComponenteJaExisteNoPacoteException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,7 +22,6 @@ import javafx.stage.Stage;
 import javafx.util.Callback;
 
 import java.util.Optional;
-
 
 public class AdicionarComponentesPacoteController {
     @FXML
